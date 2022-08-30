28f7f9ebe72e06536500a96a4f19fe3ef274a21a
Dtheran <dtheran1@gmail.com>
2022-08-30 17:57:18 -0500

    wip

--- valores.js
+++ valores.js
@@ -1,17 +1,31 @@
-//Valores primitivos
+// //Valores primitivos
 
-40  //Number
+// 40  //Number
 
-'Daniel Therán'  //String
-
-//Boolean
-true; false
-
-null
-
-undefined
+// 'Daniel Therán'  //String
+// //Boolean
+// true; false
 
 
-[1, 2, 3]
+// null
 
-{nombre: 'Daniel'}
+// undefined
+
+
+// [1, 2, 3]
+
+// {nombre: 'Daniel'}
+
+
+const pagoNomina = 2760000
+
+const pagoLuz = 170600
+const pagoTigo = 66400
+const pagoGas = 21000
+
+const pagoAdmin = 79200
+
+const totalPagosServicios = pagoGas + pagoLuz + pagoTigo;
+console.log(`Se han pagado en servicios: ${totalPagosServicios} y falta administracion ${pagoAdmin} y agua que no se cuanto es`)
+
+// const pagoFNA ?

675b2419f15d1c25d3aae01b7a9d5e396389849e
Dtheran <dtheran1@gmail.com>
2022-08-30 17:08:16 -0500

    wip

---
+++ funciones.js
@@ -0,0 +1,12 @@
+// Declarativas
+function miFuncion() {
+    return 3;
+}
+
+
+// Expresion
+const miFuncion = function(a, b){
+    return a+b;
+}
+
+miFuncion(2,3);
---
+++ valores.js
@@ -0,0 +1,17 @@
+//Valores primitivos
+
+40  //Number
+
+'Daniel Therán'  //String
+
+//Boolean
+true; false
+
+null
+
+undefined
+
+
+[1, 2, 3]
+
+{nombre: 'Daniel'}
